f61b5dbadbb4da10edda5f139f5f0b94a0e6235e
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-07-20 10:37:24 -0300

    salvando

--- src/App.java
+++ src/App.java
@@ -3,21 +3,37 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
-    /*
-     * 1. FAZER CONEXÃO HTTP E BUSCAR OS TOP 250 FILMES
-     * 2. PEGAR SÓ OS DADOS QUE INTERESSAM (TITULO, POSTER, CLASSIFICAÇÃO)
-     * 3. EXIBIR E MANIPULAR OS DADOS
+
+    // * 1. FAZER CONEXÃO HTTP E BUSCAR OS TOP 250 FILMES
+
+    /**
+     * @param args
+     * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
         URI endereco = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
+
+        // * 2. PEGAR SÓ OS DADOS QUE INTERESSAM (TITULO, POSTER, CLASSIFICAÇÃO)
+        var parcer = new JsonParcer();
+        List<Map<String, String>> listaDeFilmes = parcer.parse(body);
+
+        // * 3. EXIBIR E MANIPULAR OS DADOS
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
 
     }
 }
